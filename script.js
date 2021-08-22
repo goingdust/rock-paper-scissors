30b2c99e0487e8851e1d82ccccdbec2c44b4c43f
Dusty <dustinluck@gmail.com>
2021-08-22 13:08:44 -0400

    Cleaned up the code by following an overall line limit of 80 characters and commented an attempt at resetting the game if the user inputs a value which is not given.

--- script.js
+++ script.js
@@ -12,21 +12,36 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay().toLowerCase();
   playerSelection = prompt('Pick Rock, Paper, or Scissors!', '').toLowerCase();
+  
   if (computerSelection == playerSelection) {
-    return 'It\'s a tie!';
+    return 'You both picked the same weapon! It\'s a tie.';
   } else if (computerSelection == 'paper' && playerSelection == 'rock') {
       computerScore++;
-      return 'Computer chose Paper. Paper beats Rock! Computer wins this round. The score is Computer: ' + computerScore + ' and Player: ' + playerScore + '.';
+      return 'Computer chose Paper. \nPaper beats Rock! Computer wins this ' +
+      'round. \nThe score is Computer: ' + computerScore + ' and Player: ' + 
+      playerScore + '.';
   } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
       computerScore++;
-      return 'Computer chose Scissors. Scissors beats Paper! Computer wins this round. The score is Computer: ' + computerScore + ' and Player: ' + playerScore + '.';
+      return 'Computer chose Scissors. \nScissors beats Paper! Computer wins ' +
+      'this round. \nThe score is Computer: ' + computerScore + 
+      ' and Player: ' + playerScore + '.';
   } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
       playerScore++;
-      return 'Computer chose Paper. Scissors beats Paper! You win this round. The score is Computer: ' + computerScore + ' and Player: ' + playerScore + '.';
+      return 'Computer chose Paper. \nScissors beats Paper! You win this ' +
+      'round. \nThe score is Computer: ' + computerScore + ' and Player: ' + 
+      playerScore + '.';
   } else if (computerSelection == 'rock' && playerSelection == 'paper') {
       playerScore++;
-      return 'Computer chose Rock. Paper beats Rock! You win this round. The score is Computer: ' + computerScore + ' and Player: ' + playerScore + '.';
-  }
+      return 'Computer chose Rock. \nPaper beats Rock! You win this round. ' +
+      '\nThe score is Computer: ' + computerScore + ' and Player: ' + 
+      playerScore + '.';
+  } 
+  /* My attempt at rejecting the user's input if it's not one of the options
+  given. Gonna come back later.
+    else if (playRound() === undefined) {  
+      alert('That\s not an option silly! Be serious now, c\'mon.');
+      return playRound(playerSelection, computerSelection);
+  } */
 }
 
 function game() {
@@ -34,9 +49,11 @@ function game() {
   console.log(playRound());
   console.log(playRound());
   console.log(playRound());
+  console.log('Last round!');
   console.log(playRound());
 
-  console.log('The final scores are: Computer: ' + computerScore + ' and Player: ' + playerScore + '.');
+  console.log('The final scores are: Computer: ' + computerScore + 
+      ' and Player: ' + playerScore + '.');
 
   if (computerScore == playerScore) {
     return 'It\'s a tie game! How boring.';
