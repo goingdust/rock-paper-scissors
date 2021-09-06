b18858f459a698076572ef706e287ffdc9b1415b
Dusty <dustinluck@gmail.com>
2021-09-06 12:11:09 -0400

    added a div to display the scores and built a corresponding function to update them

--- script.js
+++ script.js
@@ -2,57 +2,67 @@ const game = () => {
 
   let computerScore = 0;
   let playerScore = 0;
+  const results = document.querySelector('.results');
 
   function playRound() {
     const options = document.querySelectorAll('.options button');
     const weapons = ['rock', 'paper', 'scissors'];
+    
 
     options.forEach(option => {
       option.addEventListener('click', function () {
         const randomWeapon = Math.floor(Math.random() * weapons.length);
         const computerSelection = weapons[randomWeapon];
         compareHands(this.textContent, computerSelection);
-        if (playerScore == 4 || computerScore == 4) {
-          return console.log('Last round!');
-        } else if (playerScore == 5 || computerScore == 5) {
-          console.log('The final scores are: Computer: ' + computerScore + 
-          ' and Player: ' + playerScore + '.');
+        updateScore(computerScore, playerScore);
+        if (playerScore == 5 || computerScore == 5) {
     
           if (computerScore == playerScore) {
-            return console.log('It\'s a tie game! How boring.');
+            return results.textContent = 'It\'s a tie game! How boring. The final scores are: Computer: ' + computerScore + 
+            ' and Player: ' + playerScore + '.';
           } else if (computerScore > playerScore) {
-            return console.log('You lose! Sucks to be you!');
+            return results.textContent = 'You lose! Sucks to be you! The final scores are: Computer: ' + computerScore + 
+            ' and Player: ' + playerScore + '.';
           } else if (computerScore < playerScore) {
-            return console.log('You win! Who would\'ve thought?!');
+            return results.textContent = 'You win! Who would\'ve thought?! The final scores are: Computer: ' + computerScore + 
+            ' and Player: ' + playerScore + '.';
           }
         } 
       });
     });
   };
 
+  function updateScore() {
+    const pScore = document.querySelector('.player-score');
+    const cScore = document.querySelector('.computer-score');
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+  }
+
   function compareHands(playerSelection, computerSelection) {
     if (computerSelection == playerSelection) {
-      return console.log('You both picked the same weapon! It\'s a tie.');
+      return results.textContent = 'You both picked the same weapon! It\'s a tie. The score is Computer: ' +
+      computerScore + ' and Player: ' + playerScore + '.';
     } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         computerScore++;
-        return console.log('Computer chose Paper. \nPaper beats Rock! Computer wins this ' +
+        return results.textContent = 'Computer chose Paper. \nPaper beats Rock! Computer wins this ' +
         'round. \nThe score is Computer: ' + computerScore + ' and Player: ' + 
-        playerScore + '.');
+        playerScore + '.';
     } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         computerScore++;
-        return console.log('Computer chose Scissors. \nScissors beats Paper! Computer wins ' +
+        return results.textContent = 'Computer chose Scissors. \nScissors beats Paper! Computer wins ' +
         'this round. \nThe score is Computer: ' + computerScore + 
-        ' and Player: ' + playerScore + '.');
+        ' and Player: ' + playerScore + '.';
     } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
         playerScore++;
-        return console.log('Computer chose Paper. \nScissors beats Paper! You win this ' +
+        return results.textContent = 'Computer chose Paper. \nScissors beats Paper! You win this ' +
         'round. \nThe score is Computer: ' + computerScore + ' and Player: ' + 
-        playerScore + '.');
+        playerScore + '.';
     } else if (computerSelection == 'rock' && playerSelection == 'paper') {
         playerScore++;
-        return console.log('Computer chose Rock. \nPaper beats Rock! You win this round. ' +
+        return results.textContent = 'Computer chose Rock. \nPaper beats Rock! You win this round. ' +
         '\nThe score is Computer: ' + computerScore + ' and Player: ' + 
-        playerScore + '.');
+        playerScore + '.';
     }
     /* My attempt at rejecting the user's input if it's not one of the options
     given. Gonna come back later.
